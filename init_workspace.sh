14a5b26d578cb7d52aac4be855e6c8f39c7c9bd2
Michael Warmke <mikew670@gmail.com>
2020-04-04 14:05:21 -0500

    added workspace init script

---
+++ init_workspace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update
+
+## apt programs install
+apt install tmux
+apt install curl
+apt install gloang-go
+apt install python3
+
+## PATH update section
+
+export PATH="~/go:$PATH"
+
+
