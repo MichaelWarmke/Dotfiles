a9892418ee658c8bcc889f1cac209c7d048a471d
Michael Warmke <mikew670@gmail.com>
2020-04-26 00:29:03 -0500

    added some config for clojure to the init script

--- init_workspace.sh
+++ init_workspace.sh
@@ -7,10 +7,25 @@ apt install tmux
 apt install curl
 apt install gloang-go
 apt install python3
+apt install openjdk-11-jdk
+apt install leiningen
+apt install maven
+
+## install clojure
+curl -O https://download.clojure.org/install/linux-install-1.10.1.536.sh
+chmod +x linux-install-1.10.1.536.sh
+sudo ./linux-install-1.10.1.536.sh
+
+## install leiningen
+mkdir -p ~/clj
+curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > ~/clj/lein.sh
+chmod 755 ~clj/lein.sh
+./lein.sh
 
 ## PATH update section
 
-export PATH="~/go:$PATH"
+export PATH="~/go/bin:$PATH"
+export PATH="~/clj:$PATH"
 
 ## create symlinks
 
